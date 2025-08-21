077b4db3ff7878191412fef01a8dd8ff0c24989c
Shyamal Chandra <sapanamicrosoftware@gmail.com>
2025-08-21 06:34:09 -0500

    Update to v1.1.3 with enhanced permission feedback

--- Formula/audio-journal-cli.rb
+++ Formula/audio-journal-cli.rb
@@ -1,9 +1,9 @@
 class AudioJournalCli < Formula
   desc "CLI tool for recording audio journal entries with location data"
   homepage "https://github.com/shyamalschandra/Audio-Journal-I"
-  version "1.1.2"
-  url "https://github.com/shyamalschandra/audio-journal-releases/releases/download/v1.1.2/audio-journal-cli"
-  sha256 "ceb9b64476e7ee6e099d8c366e25868e5c13fbdf4288ec4775262143714a4e5d"
+  version "1.1.3"
+  url "https://github.com/shyamalschandra/audio-journal-releases/releases/download/v1.1.3/audio-journal-cli"
+  sha256 "2fd91f44fb0f5024ebdf3ee9643a2e6f2e30fddbd97c918c86ec9001d33a1c02"
   # Copyright (C) 2025, Shyamal Suhana Chandra - All rights reserved
 
   depends_on :macos
