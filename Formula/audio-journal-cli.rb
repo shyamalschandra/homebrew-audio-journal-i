2650fa04f2904e7c5882635a368cc997af844d95
Shyamal Chandra <sapanamicrosoftware@gmail.com>
2025-08-21 04:24:46 -0500

    Add version field to formula

--- Formula/audio-journal-cli.rb
+++ Formula/audio-journal-cli.rb
@@ -1,6 +1,7 @@
 class AudioJournalCli < Formula
   desc "CLI tool for recording audio journal entries with location data"
   homepage "https://github.com/shyamalschandra/Audio-Journal-I"
+  version "1.0.1"
   url "https://github.com/shyamalschandra/audio-journal-releases/releases/download/v1.0.1/audio-journal-cli"
   sha256 "9943d44fa685d569f6f7baa9be2cac8816af3fe279c71ffe7a015c366fbb9be4"
   license "MIT"
